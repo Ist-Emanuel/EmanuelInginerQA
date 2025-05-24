612816fb5018266166d8583181a24b3bc2d29193
Istrate Emanuel <marin.mm758@gmail.com>
2025-05-24 15:07:36 +0300

    Update farstComit.java

--- srcEmanuel/src/DebugginExercise_Java1/farstComit.java
+++ srcEmanuel/src/DebugginExercise_Java1/farstComit.java
@@ -5,5 +5,7 @@ public class farstComit {
     public static void main(String[] args) {
 
         System.out.println("Farst commit");
+
+        System.out.println("Farst commit12432443424244");
     }
 }
